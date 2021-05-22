53922837e38d4e69d14baa70a4929f36697f7aa3
Simone Liu <simoneliu94@gmail.com>
2021-05-21 21:37:55 -0400

    Add placeholder for desktop side for now

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import mainLogo from '../assets/iPhone 12 Mockup.png';
 
 export function Desktop() {
-  return <div>You are on Desktop</div>;
+  return (
+    <div>
+      <img src={mainLogo} alt='iphone12' style={{ width: '100%' }} />
+    </div>
+  );
 }
